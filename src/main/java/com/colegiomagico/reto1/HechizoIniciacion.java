ea536bc7c74cd8af6b90c03138b010051953aa13
xiulangdelfin16-rgb <xiulangdelfin16@gmail.com>
2026-09-05 15:57:33 +0000

    feat: reto 1 solucionado

--- src/main/java/com/colegiomagico/reto1/HechizoIniciacion.java
+++ src/main/java/com/colegiomagico/reto1/HechizoIniciacion.java
@@ -6,7 +6,7 @@ package com.colegiomagico.reto1;
 public class HechizoIniciacion {
 
     public static String invocar() {
-        return ""; // TODO: Cambia esto
+        return "Xiu  - estudiante: Xiu Lang"; // TODO: Cambia esto
     }
 
     public static void main(String[] args) {
