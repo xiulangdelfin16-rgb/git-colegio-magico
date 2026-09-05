647d8efd5d1ca1779bf33aa8f1ea91b27d0f44e7
xiulangdelfin16-rgb <xiulangdelfin16@gmail.com>
2026-09-05 16:06:16 +0000

    feat: reto 2 solucionado

--- src/main/java/com/colegiomagico/reto2/Aritmancia.java
+++ src/main/java/com/colegiomagico/reto2/Aritmancia.java
@@ -6,7 +6,7 @@ package com.colegiomagico.reto2;
 public class Aritmancia {
 
     public static int sumar(int a, int b) {
-        return 0; // TODO: Cambia esto
+        return numero1 + numero2; // TODO: Cambia esto
     }
 
     public static void main(String[] args) {
