47612f1a49b9e8bfde2be12eb90e145570e9b896
xiulangdelfin16-rgb <xiulangdelfin16@gmail.com>
2026-09-06 21:09:01 +0000

    feat: reto 8 completado

--- src/main/java/com/colegiomagico/reto8/InventarioPociones.java
+++ src/main/java/com/colegiomagico/reto8/InventarioPociones.java
@@ -6,7 +6,14 @@ package com.colegiomagico.reto8;
 public class InventarioPociones {
 
     public static java.util.List<String> filtrar(java.util.List<String> ingredientes) {
-        return new java.util.ArrayList<>(); // TODO: Cambia esto
+        java.util.List<String> filtrados = new java.util.ArrayList<>();
+        for (String ingrediente : ingredientes) {
+            if (ingrediente.startsWith("A")) {
+                filtrados.add(ingrediente);
+
+                  }
+              }  
+        return filtrados; // (Use IA en su resolucion, también ayuda de mi compañera Estefa <3)
     }
 
     public static void main(String[] args) {
